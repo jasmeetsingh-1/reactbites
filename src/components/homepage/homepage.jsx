707fee09c5c2c9fd33f6b43f56560a4eb41a341f
jasmeetsingh-2 <jasmeet@mmh.homes>
2023-12-21 11:08:27 +0530

    menu bar on home page

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 import Header from "../header/header";
 import "../cssFIles/homepage/homepage.css";
+import Meals from "../../assets/data/MealsContent";
 
 export default function HomePage() {
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
   return (
     <>
       <Header />
       <div className="main-home-page-container">
-        <div>Menu Div</div>
+        <div className="outer-div-home-page-menu">
+          {Object.keys(Meals).map((item) => {
+            return (
+              <div className="home-page-menu-items">
+                <span>
+                  {capitalizeFirstLetter(item)} ({Meals[item].length})
+                </span>
+              </div>
+            );
+          })}
+
+          {/* <div className="home-page-menu-items">
+            <span>Soups (20)</span>
+          </div>
+          <div className="home-page-menu-items">
+            <span>Burgers (9)</span>
+          </div>
+          <div className="home-page-menu-items">
+            <span>Pizza (11)</span>
+          </div>
+          <div className="home-page-menu-items">
+            <span>Bowls (12)</span>
+          </div>
+          <div className="home-page-menu-items">
+            <span>Desserts (5)</span>
+          </div> */}
+        </div>
         <div>Content holder</div>
       </div>
     </>
